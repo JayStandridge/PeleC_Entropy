93af1dffd404177982da86f8d7215279b849f22d
Jon Rood <jon.rood@nrel.gov>
2022-10-28 16:05:27 -0400

    Update crusher hdf5 test run script.

--- Exec/Production/ChallengeProblem/run-crusher.sh
+++ Exec/Production/ChallengeProblem/run-crusher.sh
@@ -4,7 +4,7 @@
 #SBATCH -o %x.o%j
 #SBATCH -A CMB138_crusher
 #SBATCH -t 0:30:00
-#SBATCH -N 32
+#SBATCH -N 4
 
 set -e
 
@@ -16,4 +16,4 @@ cmd() {
 cmd "module unload PrgEnv-cray"
 cmd "module load PrgEnv-amd"
 cmd "module load rocm/5.1.0"
-cmd "srun -N32 -n256 -c1 --gpus-per-node=8 --gpu-bind=closest ./PeleC3d.hip.x86-trento.TPROF.MPI.HIP.ex challenge.inp"
+cmd "srun -N4 -n32 -c1 --gpus-per-node=8 --gpu-bind=closest ./PeleC3d.hip.x86-trento.TPROF.MPI.HIP.ex challenge.inp max_step=10 amr.max_level=1 amr.plot_int=5 ascent.plot_int=2"
